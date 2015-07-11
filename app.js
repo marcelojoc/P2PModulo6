fc245134cdb7857b00e60495887cc10e8a178e0a
marcelo <marcelojoc@gmail.com>
2015-07-11 04:23:25 -0300

    Agregados estilos responsive +layout + partials

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+// importo librerias
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
+//===================================================0
+
 
 var routes = require('./routes/index');
 
@@ -13,6 +17,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());    // agrego  el MW para los partials
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+exports.question= function(req, res){
+res.render('quiz/question',{pregunta:'Capital de Italia',
+							title: 'Quiz App'
+							} );
+};
+
+exports.answer= function(req, res){
+	if(req.query.respuesta ==='roma'){
+		res.render('quiz/answer',{respuesta:'Correcto', title: 'Quiz App' });
+	}else
+	{
+		res.render('quiz/answer',{respuesta:'Incorrecto', title: 'Quiz App'});
+	}
+};
